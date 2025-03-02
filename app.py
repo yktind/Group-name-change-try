cd6a60f326da11d3b038b5b1d96e95439a718ecc
yktind <yk7129388@gmail.com>
2025-03-02 08:10:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -140,8 +140,6 @@ def index():
     </html>
     """)
 
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
     
